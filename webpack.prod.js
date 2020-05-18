d90d2db1a8b6fbedfda171237536b9bca1b58555
cullen.ng <cullen.ng@hotmail.com>
2020-05-18 09:40:47 +0800

    update github pages

--- webpack.prod.js
+++ webpack.prod.js
@@ -10,7 +10,7 @@ module.exports = merge(common, {
 	mode: 'production',
 	output: {
 		path: path.resolve(__dirname, 'docs'),
-		publicPath: '/page-demo/',
+		publicPath: '/page-design/',
 		filename: '[name]_[hash:8].js'
 	},
 	module: {
